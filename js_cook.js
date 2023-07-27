4f2586bc1ebf634312a25cdad3b57ac989eedf69
Szymon Słoniowski <karol2016pl@gmail.com>
2023-07-27 12:22:33 +0200

    Smooth Timer + png burned + design

--- js_cook.js
+++ js_cook.js
@@ -49,9 +49,7 @@
 // }
 
 const timers = {
-  // BFish: { timer1: 10, timer2: 12 },
-  // [Down here is normal fish timers]
-  BFish: { timer1: 45, timer2: 35 },
+  BFish: { timer1: 5, timer2: 5 },
   BMeat: { timer1: 65, timer2: 55 },
   BTFish: { timer1: 95, timer2: 85 },
   BMonsterM: { timer1: 125, timer2: 115 },
@@ -60,22 +58,24 @@ const timers = {
 const circle = document.getElementById('circle');
 let countdownInterval;
 let phase = 1;
+let timerStart;
+let timer;
 
 function startCountdown(id) {
-  let timer = timers[id].timer1;
-  let totalDuration = timer;
-  
+  timer = timers[id].timer1;
+
   if (phase === 1) {
     timer = timers[id].timer1;
-    totalDuration = timer;
     circle.style.visibility = 'visible';
   } else if (phase === 2) {
     timer = timers[id].timer2;
-    totalDuration = timer;
     circle.style.strokeDashoffset = '440';
     circle.style.visibility = 'visible';
   }
-  let initialProgress = timer / totalDuration;
+  
+  let totalDuration = timer;
+  timerStart = Date.now();
+  let initialProgress = timer / (totalDuration * 60);
   let initialDashoffsetValue = initialProgress * 440; 
   circle.style.strokeDashoffset = initialDashoffsetValue;
   let tip;
@@ -101,55 +101,93 @@ function startCountdown(id) {
   const infoElement = document.getElementById("info");
   const timerElement = document.getElementById("timer");
   const info2Element = document.getElementById("info2");
+  const burnedImage = document.getElementById("burnedImage");
 
-  infoElement.innerText = `Wait for ${name} to get perfectly cooked`;
+  infoElement.innerText = `Wait for ${name} to get perfectly cooked ♨️`;
   timerElement.innerText = timer;
-  info2Element.innerText = `[Tip]: ${tip}`;
+  info2Element.innerText = `[Tip] ${tip} [Tip]`;
   timer--;
-
+  startTime = Date.now();
   countdownInterval = setInterval(() => {
-    if (timer === 9 && phase === 1) {
+    if (Math.floor((totalDuration*1000 - (Date.now() - startTime))/1000) === 9 && phase === 1) {
       playSound1();
-    } else if (timer === 9 && phase === 2) {
+    } else if (Math.floor((totalDuration*1000 - (Date.now() - startTime))/1000) === 9 && phase === 2) {
       playSound2();
     }
-
-    if (timer === 0) {
+    if (Date.now() - startTime >= totalDuration * 1000) {
       clearInterval(countdownInterval);
       phase++;
 
       if (phase === 3) {
-        infoElement.innerText = `Your ${name} has burned!\n\n`;
+        infoElement.innerText = `Your ${name} has burned :C\n\n`;
         timerElement.style.display = "none";
         info2Element.innerText = "";
         circle.style.strokeDashoffset = '440';
         circle.style.visibility = 'hidden';
+        // Pokazujemy obraz
+        burnedImage.style.visibility = 'visible';
         phase = 1;
       } else if (phase === 2) {
-        timer = timers[id].timer2;
-        totalDuration = timer;
+        totalDuration = timers[id].timer2;
         startCountdown(id);
-        infoElement.innerText = `Quick take your ${name} before it burns!`;
+        infoElement.innerText = `Quick take your ${name} before it burns 💢`;
         timerElement.style.display = "inline";
         info2Element.innerText = "";
       }
-    }
+    } 
     
-    let progress = timer/totalDuration;
+    let progress = timer/(totalDuration*60);
     let dashoffsetValue = progress * 440; 
 
     circle.style.strokeDashoffset = dashoffsetValue;
 
-    timerElement.innerText = timer;
+    timerElement.innerText = Math.ceil((totalDuration*1000 - (Date.now() - startTime))/1000);
     timer--;
-  }, 1000);
+  }, 1000); 
+
+  function updateCircle() {
+    let progress = 1 - (Date.now() - startTime) / (totalDuration * 1000);
+    let dashoffsetValue = progress * 440; 
+    circle.style.strokeDashoffset = dashoffsetValue;
+
+    if (Date.now() - startTime < totalDuration * 1000) {
+      requestAnimationFrame(updateCircle);
+    }
+  }
+  requestAnimationFrame(updateCircle);
 }
 
+function updateCircle() {
+  let elapsed = (Date.now() - timerStart) / 1000;
+  let remaining = timer - elapsed;
 
+  let progress = remaining / timer;
+  let dashoffsetValue = progress * 440; 
+  circle.style.strokeDashoffset = dashoffsetValue;
 
+  if (remaining <= 0) {
+    clearInterval(countdownInterval);
+    phase++;
 
-
-
+    if (phase === 3) {
+      infoElement.innerText = `Your ${name} has burned!\n\n`;
+      timerElement.style.display = "none";
+      info2Element.innerText = "";
+      circle.style.strokeDashoffset = '440';
+      circle.style.visibility = 'hidden';
+      // Pokazujemy obraz
+      burnedImage.style.visibility = 'visible';
+      phase = 1;
+    } else if (phase === 2) {
+      startCountdown(id);
+      infoElement.innerText = `Quick take your ${name} before it burns!`;
+      timerElement.style.display = "inline";
+      info2Element.innerText = "";
+    }
+  } else {
+    requestAnimationFrame(updateCircle);
+  }
+}
 
 const audio1 = new Audio("END-ALARM-1.mp3");
 const audio2 = new Audio("END-ALARM-2.mp3");
@@ -175,6 +213,7 @@ function openModal() {
   modal.style.display = 'flex';
   blurBackground.style.display = 'block'; // Pokazujemy element blur-background
 }
+
 const buttons = document.querySelectorAll("nav button");
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", function () {
@@ -188,8 +227,11 @@ for (let i = 0; i < buttons.length; i++) {
 function closeModal() {
   var modal = document.getElementById('modal');
   var blurBackground = document.getElementById('blur-background');
+  var burnedImage = document.getElementById("burnedImage"); // pobieramy element
   modal.style.display = 'none';
   blurBackground.style.display = 'none'; // Ukrywamy element blur-background
+  // Ukrywamy obraz
+  burnedImage.style.visibility = 'hidden';
 }
 
 document.getElementById("close").addEventListener("click", function () {
