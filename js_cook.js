d97b1937e6b7c671e2d52fdc7875edb133762ae0
Szymon Słoniowski <karol2016pl@gmail.com>
2023-07-27 14:14:00 +0200

    Comments + deleted duplicated code

--- js_cook.js
+++ js_cook.js
@@ -48,22 +48,40 @@
 //   }
 // }
 
+
+// const timers = {
+//   BFish: { timer1: 11, timer2: 11, name: "Fish", tip: "Wait for the eyes to turn completely white" },
+//   BMeat: { timer1: 65, timer2: 55, name: "Basic Meat", tip: "Wait for the entire piece to turn brown. No pink should be visible" },
+//   BTFish: { timer1: 95, timer2: 85, name: "Trophy Fish", tip: "Wait for the eyes to turn completely white" },
+//   BMonsterM: { timer1: 125, timer2: 115, name: "Kraken / Megalodon", tip: "Wait for the entire piece to turn brown. No pink should be visible" },
+// };
+
+// Definicja czasów przygotowania dla różnych produktów
 const timers = {
-  BFish: { timer1: 5, timer2: 5 },
+  BFish: { timer1: 11, timer2: 11 },
   BMeat: { timer1: 65, timer2: 55 },
   BTFish: { timer1: 95, timer2: 85 },
   BMonsterM: { timer1: 125, timer2: 115 },
 };
-
+// Pobieramy referencję do elementu okręgu na stronie
 const circle = document.getElementById('circle');
-let countdownInterval;
-let phase = 1;
-let timerStart;
-let timer;
 
+// Inicjalizacja dźwięków ostrzegawczych
+const audio1 = new Audio("END-ALARM-1.mp3");
+const audio2 = new Audio("END-ALARM-2.mp3");
+
+// Inicjalizujemy zmienne
+let countdownInterval; // Zmienna do przechowywania interwału odliczania
+let phase = 1; // Aktualna faza przygotowania
+let timerStart; // Czas rozpoczęcia odliczania
+let timer; // Czas pozostały do zakończenia odliczania
+
+// Funkcja do rozpoczęcia odliczania
 function startCountdown(id) {
+// Pobieramy odpowiedni czas przygotowania w zależności od fazy
   timer = timers[id].timer1;
 
+// Ustawiamy czas oraz widoczność okręgu na ekranie w zależności od fazy
   if (phase === 1) {
     timer = timers[id].timer1;
     circle.style.visibility = 'visible';
@@ -73,21 +91,27 @@ function startCountdown(id) {
     circle.style.visibility = 'visible';
   }
   
+  // Obliczamy całkowity czas trwania odliczania
   let totalDuration = timer;
+
+  // Pobieramy czas rozpoczęcia odliczania
   timerStart = Date.now();
+
+  // Ustawiamy początkowy postęp na okręgu
   let initialProgress = timer / (totalDuration * 60);
   let initialDashoffsetValue = initialProgress * 440; 
   circle.style.strokeDashoffset = initialDashoffsetValue;
-  let tip;
 
+  // Określamy wskazówki dla danego produktu
+  let tip;
   if (id === "BFish" || id === "BTFish") {
     tip = "Wait for the eyes to turn completely white";
   } else {
     tip = "Wait for the entire piece to turn brown. No pink should be visible";
   }
 
+  // Określamy nazwę danego produktu
   let name;
-
   if (id === "BTFish") {
     name = "Trophy Fish";
   } else if (id === "BMonsterM") {
@@ -98,33 +122,39 @@ function startCountdown(id) {
     name = "Fish";
   }
 
+  // Pobieramy referencje do elementów na stronie, w których wyświetlimy informacje
   const infoElement = document.getElementById("info");
   const timerElement = document.getElementById("timer");
   const info2Element = document.getElementById("info2");
   const burnedImage = document.getElementById("burnedImage");
 
+  // Wyświetlamy informacje o danym produkcie i czasie przygotowania
   infoElement.innerText = `Wait for ${name} to get perfectly cooked ♨️`;
   timerElement.innerText = timer;
-  info2Element.innerText = `[Tip] ${tip} [Tip]`;
+  info2Element.innerText = `[Tip] ${tip}`;
+
+   // Rozpoczynamy odliczanie oraz ustawiamy interwał odświeżania ekranu co 1 sekundę
   timer--;
   startTime = Date.now();
   countdownInterval = setInterval(() => {
-    if (Math.floor((totalDuration*1000 - (Date.now() - startTime))/1000) === 9 && phase === 1) {
+    if (Math.floor((totalDuration*1000 - (Date.now() - startTime))/1000) === 8 && phase === 1) {
       playSound1();
-    } else if (Math.floor((totalDuration*1000 - (Date.now() - startTime))/1000) === 9 && phase === 2) {
+    } else if (Math.floor((totalDuration*1000 - (Date.now() - startTime))/1000) === 8 && phase === 2) {
       playSound2();
     }
+    // Sprawdzamy, czy odliczanie dobiegło końca
     if (Date.now() - startTime >= totalDuration * 1000) {
       clearInterval(countdownInterval);
       phase++;
 
+       // W zależności od fazy wyświetlamy odpowiednie informacje
       if (phase === 3) {
         infoElement.innerText = `Your ${name} has burned :C\n\n`;
         timerElement.style.display = "none";
         info2Element.innerText = "";
         circle.style.strokeDashoffset = '440';
+        // Pokazujemy obrazek, który symbolizuje przypalony produkt
         circle.style.visibility = 'hidden';
-        // Pokazujemy obraz
         burnedImage.style.visibility = 'visible';
         phase = 1;
       } else if (phase === 2) {
@@ -135,16 +165,16 @@ function startCountdown(id) {
         info2Element.innerText = "";
       }
     } 
-    
+    // Obliczamy postęp na okręgu oraz aktualizujemy pozostały czas na ekranie
     let progress = timer/(totalDuration*60);
     let dashoffsetValue = progress * 440; 
-
     circle.style.strokeDashoffset = dashoffsetValue;
-
     timerElement.innerText = Math.ceil((totalDuration*1000 - (Date.now() - startTime))/1000);
     timer--;
-  }, 1000); 
 
+  }, 1000); 
+  
+// Funkcja do aktualizacji postępu na okręgu w czasie rzeczywistym
   function updateCircle() {
     let progress = 1 - (Date.now() - startTime) / (totalDuration * 1000);
     let dashoffsetValue = progress * 440; 
@@ -157,6 +187,7 @@ function startCountdown(id) {
   requestAnimationFrame(updateCircle);
 }
 
+// Funkcja do aktualizacji postępu na okręgu podczas odliczania w fazie 2
 function updateCircle() {
   let elapsed = (Date.now() - timerStart) / 1000;
   let remaining = timer - elapsed;
@@ -164,33 +195,9 @@ function updateCircle() {
   let progress = remaining / timer;
   let dashoffsetValue = progress * 440; 
   circle.style.strokeDashoffset = dashoffsetValue;
-
-  if (remaining <= 0) {
-    clearInterval(countdownInterval);
-    phase++;
-
-    if (phase === 3) {
-      infoElement.innerText = `Your ${name} has burned!\n\n`;
-      timerElement.style.display = "none";
-      info2Element.innerText = "";
-      circle.style.strokeDashoffset = '440';
-      circle.style.visibility = 'hidden';
-      // Pokazujemy obraz
-      burnedImage.style.visibility = 'visible';
-      phase = 1;
-    } else if (phase === 2) {
-      startCountdown(id);
-      infoElement.innerText = `Quick take your ${name} before it burns!`;
-      timerElement.style.display = "inline";
-      info2Element.innerText = "";
-    }
-  } else {
-    requestAnimationFrame(updateCircle);
-  }
 }
 
-const audio1 = new Audio("END-ALARM-1.mp3");
-const audio2 = new Audio("END-ALARM-2.mp3");
+
 
 function playSound1() {
   audio1.play();
@@ -207,6 +214,7 @@ function stopSound() {
   audio2.currentTime = 0;
 }
 
+// Funkcja otwierająca modal
 function openModal() {
   var modal = document.getElementById('modal');
   var blurBackground = document.getElementById('blur-background');
@@ -214,6 +222,7 @@ function openModal() {
   blurBackground.style.display = 'block'; // Pokazujemy element blur-background
 }
 
+// Dodanie listenera do wszystkich przycisków w nawigacji
 const buttons = document.querySelectorAll("nav button");
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", function () {
@@ -224,6 +233,7 @@ for (let i = 0; i < buttons.length; i++) {
   });
 }
 
+// Funkcja zamykająca modal
 function closeModal() {
   var modal = document.getElementById('modal');
   var blurBackground = document.getElementById('blur-background');
@@ -234,6 +244,7 @@ function closeModal() {
   burnedImage.style.visibility = 'hidden';
 }
 
+// Dodanie listenera do przycisku "close" w modalu
 document.getElementById("close").addEventListener("click", function () {
   closeModal()
   document.getElementById("modal").style.display = "none";
