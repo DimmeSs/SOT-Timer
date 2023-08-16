af07eeaec7348e891cf684055f569615b695ec75
Szymon Słoniowski <karol2016pl@gmail.com>
2023-08-16 15:41:11 +0200

    Slider Fixed

--- container2.js
+++ container2.js
@@ -1,48 +1,58 @@
 let currentSlide = 1;
-const totalSlides = 6; 
-const slideChangeInterval = 5000;  
+const totalSlides = 6;
+const slideChangeInterval = 5000;
 
 function changeSlide() {
-    currentSlide++;
-    if (currentSlide > totalSlides) {
-        currentSlide = 1;
-    }
+  currentSlide++;
+  if (currentSlide > totalSlides) {
+    currentSlide = 1;
+  }
+  showSlide(currentSlide);
+}
 
-    // Przejście do odpowiedniego slajdu
-    const slideElement = document.getElementById(`slide-${currentSlide}`);
-    slideElement.scrollIntoView({behavior: "smooth", block: "nearest", inline: "start"});
+function showSlide(slideNumber) {
+  const slider = document.querySelector('.slider');
+  const slideWidth = slider.scrollWidth / totalSlides;
+  slider.scrollLeft = (slideNumber - 1) * slideWidth;
 }
 
 // Rozpoczyna automatyczne przejście między slajdami
 setInterval(changeSlide, slideChangeInterval);
 
+const sliderNavLinks = document.querySelectorAll('.slider-nav a');
+sliderNavLinks.forEach((link, index) => {
+  link.addEventListener('click', function(event) {
+    event.preventDefault();
+    showSlide(index + 1);
+  });
+});
 
 const listItems = document.querySelectorAll('li[data-src]');
 const overlay = document.getElementById('overlay');
 const overlayImage = document.getElementById('overlayImage');
 
 listItems.forEach(item => {
-    // Dodajemy nasłuchiwanie do tekstu wewnątrz li oraz do elementu <b>
-    const textNodes = Array.from(item.childNodes).filter(node => node.nodeType === 3 || node.nodeName === "B");
-    textNodes.forEach(textNode => {
-        textNode.addEventListener('mouseenter', function() {
-            const imagePath = item.getAttribute('data-src');
-            overlayImage.src = imagePath;
-            overlay.style.display = 'flex';
-        });
-
-        textNode.addEventListener('mouseleave', function() {
-            overlay.style.display = 'none';
-        });
+  const textNodes = Array.from(item.childNodes).filter(node => node.nodeType === 3 || node.nodeName === "B");
+  textNodes.forEach(textNode => {
+    textNode.addEventListener('mouseenter', function() {
+      const imagePath = item.getAttribute('data-src');
+      overlayImage.src = imagePath;
+      overlay.style.display = 'flex';
     });
+
+    textNode.addEventListener('mouseleave', function() {
+      overlay.style.display = 'none';
+    });
+  });
 });
 
 overlay.addEventListener('mouseenter', function() {
-    overlay.style.display = 'none';
+  overlay.style.display = 'none';
 });
 
 document.addEventListener('contextmenu', function(e) {
   if (e.target.tagName === 'IMG') {
-      e.preventDefault();
+    e.preventDefault();
   }
 });
+
