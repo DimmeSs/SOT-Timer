f9eee7a46527bd7c9a3e22b99df1ea1d47dc11e3
Szymon Słoniowski <karol2016pl@gmail.com>
2023-08-16 23:51:01 +0200

    Fix Preload.js v5

--- public/preloader-animation.js
+++ public/preloader-animation.js
@@ -3,28 +3,28 @@ document.addEventListener("DOMContentLoaded", function () {
   var preloader = document.getElementById("preloader");
   var content = document.getElementById("content");
 
-  // After 3 seconds, fade out the preloader and fade in the content
-  setTimeout(function () {
+
+  (async () => {
+    const wait = (n) => new Promise((res) => setTimeout(res, n));
+    const loadImg = () =>
+    new Promise((res) => {
+        const el = document.querySelector("#section-0");
+        const imgToLoad = document.createElement("img");
+        imgToLoad.src = el.src;
+        imgToLoad.addEventListener("load", ()=>{
+          el.replaceWith(imgToLoad);
+          res();
+        });
+    });
+
+    await Promise.all([wait(3000), loadImg()]);
+    console.log("Loaded");
     preloader.style.opacity = "0"; /* Zmniejszamy przezroczystość na 0 */
     preloader.style.pointerEvents =
       "none"; /* Wyłączamy interakcję z preloaderem */
 
     content.style.opacity =
       "1"; /* Pokazujemy zawartość strony z przezroczystością 1 */
-  }, 2000);
-  //⬆️2000
-
-  (async () => {
-    const wait = (n) => new Promise((res) => setTimeout(res, n));
-    const loadImg = () =>
-      new Promise((res) => {
-        document
-          .querySelector(".section-0 img")
-          .addEventListener("load", () => {
-            res();
-          });
-      });
-
-    await Promise.all([wait(3000), loadImg()]);
+    preloader.style.display = "none";
   })();
 });
