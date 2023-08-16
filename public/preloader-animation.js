98467a3c2e5604e4a4bd529d82febe5a2fb4c1e4
Szymon Słoniowski <karol2016pl@gmail.com>
2023-08-16 23:37:16 +0200

    Fix Preload.js v4

--- public/preloader-animation.js
+++ public/preloader-animation.js
@@ -1,20 +1,30 @@
+// Wait for the DOM to be fully loaded
+document.addEventListener("DOMContentLoaded", function () {
+  var preloader = document.getElementById("preloader");
+  var content = document.getElementById("content");
 
-  // Wait for the DOM to be fully loaded
-  document.addEventListener("DOMContentLoaded", function() {
-    var preloader = document.getElementById("preloader");
-    var content = document.getElementById("content");
+  // After 3 seconds, fade out the preloader and fade in the content
+  setTimeout(function () {
+    preloader.style.opacity = "0"; /* Zmniejszamy przezroczystość na 0 */
+    preloader.style.pointerEvents =
+      "none"; /* Wyłączamy interakcję z preloaderem */
 
-    // After 3 seconds, fade out the preloader and fade in the content
-    setTimeout(function() {
-      preloader.style.opacity = "0"; /* Zmniejszamy przezroczystość na 0 */
-      preloader.style.pointerEvents = "none"; /* Wyłączamy interakcję z preloaderem */
-
-      content.style.opacity = "1"; /* Pokazujemy zawartość strony z przezroczystością 1 */
-    },2000);
+    content.style.opacity =
+      "1"; /* Pokazujemy zawartość strony z przezroczystością 1 */
+  }, 2000);
   //⬆️2000
 
-    setTimeout(function() {
-      preloader.style.display = "none"; /* Ukrywamy preloader */
-    },3000);
-  //⬆️3000 
-  });
+  (async () => {
+    const wait = (n) => new Promise((res) => setTimeout(res, n));
+    const loadImg = () =>
+      new Promise((res) => {
+        document
+          .querySelector(".section-0 img")
+          .addEventListener("load", () => {
+            res();
+          });
+      });
+
+    await Promise.all([wait(3000), loadImg()]);
+  })();
+});
